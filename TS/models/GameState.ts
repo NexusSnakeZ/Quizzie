5e295c0dae57b39a37d3a840b66571382ad101d0
NexusSnakeZ <101937@glr.nl>
2026-06-17 02:44:46 +0200

    Add GameState enum for quiz state management

---
+++ TS/models/GameState.ts
@@ -0,0 +1,8 @@
+/**
+ * Game state enum for quiz state management
+ */
+export enum GameState {
+    INTRO = "INTRO",           // Show introduction
+    QUESTION = "QUESTION",     // Show questions
+    END = "END"               // Show results
+}
